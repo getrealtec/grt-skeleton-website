d935eaf28b450c284fb77a2bf6d693784a93b43b
David Tremblay <david.tremblay@getrealtec.com>
2019-05-16 20:08:37 -0400

    Finished the language module. It is now fully usable

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route({
+     *     "en": "/default",
+     *     "fr": "/defaut",
+     *     "es": "/defecto"
+     *   }, name="default"
+     * )
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
